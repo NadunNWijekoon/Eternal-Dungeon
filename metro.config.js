b667bf1f9a799955569aa89bb67b78ed73b50e9b
Nadun N. Wijekoon <115790306+NadunNWijekoon@users.noreply.github.com>
2026-04-03 15:05:32 +0530

    feat: remove iOS project configuration from metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,10 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Ensure assets are properly handled and not double-processed
-config.project.ios = {
-  project: __dirname + '/ios',
-};
-
 config.project.android = {
   project: __dirname + '/android',
 };
