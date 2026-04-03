94ebaf8c2a8977678acd3089c9c1c44533657048
Nadun N. Wijekoon <115790306+NadunNWijekoon@users.noreply.github.com>
2026-04-03 15:08:24 +0530

    feat: remove unused iOS configuration and asset handling from metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,27 +1,36 @@
+// const { getDefaultConfig } = require('expo/metro-config');
+
+// const config = getDefaultConfig(__dirname);
+
+// // Ensure assets are properly handled and not double-processed
+// config.project.ios = {
+//   project: __dirname + '/ios',
+// };
+
+// config.project.android = {
+//   project: __dirname + '/android',
+// };
+
+// // Exclude problematic node_modules and build artifacts
+// config.resolver.blockList = [
+//   /node_modules\/.*\/node_modules/,
+//   /gradle/,
+//   /.gradle/,
+// ];
+
+// // Optimize asset loading
+// config.resolver.assetExts = [
+//   ...config.resolver.assetExts.filter(ext => ext !== 'png'),
+//   'png',
+//   'jpg',
+//   'jpeg',
+//   'gif',
+//   'webp',
+// ];
+
+// module.exports = config;
+
+
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-// Ensure assets are properly handled and not double-processed
-config.project.android = {
-  project: __dirname + '/android',
-};
-
-// Exclude problematic node_modules and build artifacts
-config.resolver.blockList = [
-  /node_modules\/.*\/node_modules/,
-  /gradle/,
-  /.gradle/,
-];
-
-// Optimize asset loading
-config.resolver.assetExts = [
-  ...config.resolver.assetExts.filter(ext => ext !== 'png'),
-  'png',
-  'jpg',
-  'jpeg',
-  'gif',
-  'webp',
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
